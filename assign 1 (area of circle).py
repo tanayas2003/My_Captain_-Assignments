c62a3c307a5fc7f472722dddbece568f954ace2d
tanayas2003 <110730855+tanayas2003@users.noreply.github.com>
2022-08-13 14:21:18 +0530

    Add files via upload

---
+++ assign 1 (area of circle).py
@@ -0,0 +1,12 @@
+#TANAYA SAWAJI
+"""
+assignment 1 : Find the area of the circle
+"""
+
+CONSTANT_PI = 3.14
+radius = float(input("enter the radius of the circle :"))
+area = CONSTANT_PI * radius**2 
+
+
+print("the area of the circle is :", area)
+input("End of the program")
