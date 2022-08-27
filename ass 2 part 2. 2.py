756f146f41642ab3d129b8bf78188984612f3d65
tanayas2003 <110730855+tanayas2003@users.noreply.github.com>
2022-08-27 20:31:18 +0530

    Add files via upload

---
+++ ass 2 part 2. 2.py
@@ -0,0 +1,14 @@
+# tanaya
+"""
+ass 2 part 2
+"""
+
+nums = (12, -7, 5, 64, -14)
+print("Original numbers in the list:", nums)
+new_nums = list(filter(lambda x: x >0, nums))
+print("Positive numbers in the list:", new_nums)
+
+nums = (12, 14, -95, 3)
+print("Original numbers in the list:", nums)
+new_nums = list(filter(lambda x: x >0, nums))
+print("Positive numbers in the list:", new_nums)
